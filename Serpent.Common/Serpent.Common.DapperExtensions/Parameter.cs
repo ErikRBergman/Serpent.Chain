63665230c341b74b9b067a3012c693996e94d78d
Erik Bergman <erik_r_bergman@hotmail.com>
2017-08-14 13:14:46 +0200

    Added an extension to dynamic parameters. Experimental!

---
+++ Serpent.Common/Serpent.Common.DapperExtensions/Parameter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using Dapper;
+
+namespace Serpent.Common.DapperExtensions
+{
+    public struct Parameter
+    {
+        public Parameter(string name, object value, DbType? dbType, ParameterDirection? direction, int? size, byte? precision, byte? scale)
+        {
+            this.Name = name;
+            this.Value = value;
+            this.DbType = dbType;
+            this.Direction = direction;
+            this.Size = size;
+            this.Precision = precision;
+            this.Scale = scale;
+        }
+
+        public Parameter(string name, object value)
+        {
+            this.Name = name;
+            this.Value = value;
+            this.Value = null;
+            this.DbType = null;
+            this.Direction = null;
+            this.Size = null;
+            this.Precision = null;
+            this.Scale = null;
+        }
+
+        public string Name { get; set; }
+
+        public object Value { get; set; }
+
+        public DbType? DbType { get; set; }
+
+        public ParameterDirection? Direction { get; set; }
+
+        public int? Size { get; set; }
+
+        public byte? Precision { get; set; }
+
+        public byte? Scale { get; set; }
+
+    }
+}
