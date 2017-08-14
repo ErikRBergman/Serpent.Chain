691aabf552771496dc49abff6706aa4377a24317
Erik Bergman <erik_r_bergman@hotmail.com>
2017-08-14 14:18:46 +0200

    Added som parallel async functions

---
+++ Serpent.Common/Serpent.Common.Async/ParallelForEach.cs
@@ -0,0 +1,74 @@
+﻿namespace Serpent.Common.Async
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public static class Parallel
+    {
+        private static readonly int ProcessorCount = Environment.ProcessorCount;
+
+        public static Task ForEachAsync<T>(this IEnumerable<T> items, Func<T, Task> workerFunc, int concurrencyLevel)
+        {
+            return ForEachAsync(items, workerFunc, concurrencyLevel, CancellationToken.None);
+        }
+
+        public static Task ForEachAsync<T>(this IEnumerable<T> items, Func<T, Task> workerFunc)
+        {
+            return ForEachAsync(items, workerFunc, ProcessorCount * 4, CancellationToken.None);
+        }
+
+        public static Task ForEachAsync<T>(this IEnumerable<T> items, Func<T, Task> workerFunc, int concurrencyLevel, CancellationToken cancellationToken)
+        {
+            if (concurrencyLevel < 1)
+            {
+                throw new ArgumentException("concurrencyCount may not be less than 1");
+            }
+
+            var queue = new ConcurrentQueue<T>(items);
+
+            return Task.WhenAll(Enumerable.Range(0, concurrencyLevel).Select(v => Task.Run(() => ForEachAsyncWorker(queue, workerFunc, cancellationToken), cancellationToken)));
+        }
+
+        public static Task ForEachAsync<TItem, TContext>(this IEnumerable<TItem> items, TContext context, Func<TItem, TContext, CancellationToken, Task> workerFunc, int concurrencyLevel, CancellationToken cancellationToken)
+        {
+            if (concurrencyLevel < 1)
+            {
+                throw new ArgumentException("concurrencyCount may not be less than 1");
+            }
+
+            var queue = new ConcurrentQueue<TItem>(items);
+
+            return Task.WhenAll(Enumerable.Range(0, concurrencyLevel).Select(v => Task.Run(() => ForEachAsyncWorker(queue, context, workerFunc, cancellationToken), cancellationToken)));
+        }
+
+        private static async Task ForEachAsyncWorker<T>(ConcurrentQueue<T> queue, Func<T, Task> workerFunc, CancellationToken cancellationToken)
+        {
+            while (queue.TryDequeue(out var queueItem))
+            {
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    return;
+                }
+
+                await workerFunc(queueItem).ConfigureAwait(false);
+            }
+        }
+
+        private static async Task ForEachAsyncWorker<TItem, TContext>(ConcurrentQueue<TItem> queue, TContext context, Func<TItem, TContext, CancellationToken, Task> workerFunc, CancellationToken cancellationToken)
+        {
+            while (queue.TryDequeue(out var queueItem))
+            {
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    return;
+                }
+
+                await workerFunc(queueItem, context, cancellationToken).ConfigureAwait(false);
+            }
+        }
+    }
+}
---
+++ Serpent.Common/Serpent.Common.Async/ParallelQueueWorker.cs
@@ -0,0 +1,137 @@
+﻿namespace Serpent.Common.Async
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class ParallelQueueWorker<T>
+    {
+        private readonly int concurrencyLevel;
+
+        private readonly object lockObject = new object();
+
+        private readonly ConcurrentQueue<T> queue = new ConcurrentQueue<T>();
+
+        private readonly SemaphoreSlim semaphoreSlim = new SemaphoreSlim(0);
+
+        private readonly Func<T, CancellationToken, Task> workerFunc;
+
+        private CancellationTokenSource cancellationTokenSource;
+
+        private int isStarted;
+
+        private IReadOnlyCollection<Task> workerTasks;
+
+        public ParallelQueueWorker(Func<T, CancellationToken, Task> workerFunc, IEnumerable<T> items = null, int concurrencyLevel = 16)
+        {
+            if (concurrencyLevel < 1)
+            {
+                throw new ArgumentException("concurrencyLevel may not be less than 1");
+            }
+
+            this.workerFunc = workerFunc ?? throw new ArgumentNullException(nameof(workerFunc));
+
+            this.concurrencyLevel = concurrencyLevel;
+            if (items != null)
+            {
+                this.EnqueueRange(items);
+            }
+        }
+
+        public int Count => this.queue.Count;
+
+        public bool IsRunning => this.isStarted == 1;
+
+        public void Enqueue(T item)
+        {
+            this.queue.Enqueue(item);
+            this.semaphoreSlim.Release();
+        }
+
+        public void EnqueueRange(IEnumerable<T> items)
+        {
+            var count = 0;
+
+            foreach (var item in items)
+            {
+                this.queue.Enqueue(item);
+                count++;
+            }
+
+            this.semaphoreSlim.Release(count);
+        }
+
+        public void Start()
+        {
+            lock (this.lockObject)
+            {
+                if (Interlocked.CompareExchange(ref this.isStarted, 1, 0) == 0)
+                {
+                    this.workerTasks = new List<Task>(this.concurrencyLevel);
+                    this.cancellationTokenSource = new CancellationTokenSource();
+
+                    var tasks = new List<Task>(this.concurrencyLevel);
+
+                    for (var i = 0; i < this.concurrencyLevel; i++)
+                    {
+                        tasks.Add(Task.Run(this.WorkerMethodAsync));
+                    }
+
+                    this.workerTasks = tasks;
+                }
+                else
+                {
+                    throw new Exception("Worker may not be started more than once");
+                }
+            }
+        }
+
+        public Task StopAsync()
+        {
+            lock (this.lockObject)
+            {
+                var tasks = this.workerTasks;
+                var tokenSource = this.cancellationTokenSource;
+
+                if (Interlocked.CompareExchange(ref this.isStarted, 0, 0) == 0)
+                {
+                    this.workerTasks = null;
+                    this.cancellationTokenSource = null;
+
+                    tokenSource.Cancel();
+                    return Task.WhenAll(tasks);
+                }
+                else
+                {
+                    throw new Exception("Worker is not running");
+                }
+            }
+        }
+
+        private async Task WorkerMethodAsync()
+        {
+            CancellationToken token;
+            lock (this.lockObject)
+            {
+                token = this.cancellationTokenSource.Token;
+            }
+
+            while (true)
+            {
+                await this.semaphoreSlim.WaitAsync(token);
+
+                if (token.IsCancellationRequested)
+                {
+                    break;
+                }
+
+                if (this.queue.TryDequeue(out var item))
+                {
+                    await this.workerFunc(item, token);
+                }
+            }
+        }
+    }
+}
