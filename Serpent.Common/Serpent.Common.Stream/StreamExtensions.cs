265759245989ab5243dbe412f9e6310b6bac6ffb
Erik Bergman <erik_r_bergman@hotmail.com>
2017-08-14 15:31:18 +0200

    Added stream project

---
+++ Serpent.Common/Serpent.Common.Stream/StreamExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace Serpent.Common.Stream
+{
+    using System.IO;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public static class StreamExtensions
+    {
+        public static async Task<Stream> GetMemoryStreamCopyAsync(this Stream stream)
+        {
+            var memoryStream = new MemoryStream((int)stream.Length);
+            await stream.CopyToAsync(memoryStream);
+            memoryStream.Position = 0;
+            return memoryStream;
+        }
+
+        public static async Task<Stream> GetMemoryStreamCopyAsync(this Stream stream, CancellationToken cancellationToken)
+        {
+            var memoryStream = new MemoryStream((int)stream.Length);
+            await stream.CopyToAsync(memoryStream, 81920, cancellationToken);
+            memoryStream.Position = 0;
+            return memoryStream;
+        }
+    }
+}
