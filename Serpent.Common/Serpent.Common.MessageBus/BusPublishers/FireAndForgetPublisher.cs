c6196f9ff428a07514daa459131df3451f04a250
Erik Bergman <erik_r_bergman@hotmail.com>
2017-09-13 16:39:33 +0200

    The fire and forget publisher was forgotten in the stage ;)

---
+++ Serpent.Common/Serpent.Common.MessageBus/BusPublishers/FireAndForgetPublisher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Serpent.Common.MessageBus.BusPublishers
+{
+    using System.Threading.Tasks;
+
+    public class FireAndForgetPublisher<T> : BusPublisher<T>
+    {
+        private readonly BusPublisher<T> innerPublisher;
+
+        public FireAndForgetPublisher(BusPublisher<T> innerPublisher)
+        {
+            this.innerPublisher = innerPublisher;
+        }
+
+        public override Task PublishAsync(IEnumerable<ISubscription<T>> subscriptions, T message)
+        {
+            Task.Run(() => this.innerPublisher.PublishAsync(subscriptions, message)));
+            return Task.CompletedTask;
+        }
+    }
+}
