1141cdf682b8a80a37787d7e10d98418d3d44d30
Erik Bergman <erik_r_bergman@hotmail.com>
2017-09-13 16:40:49 +0200

    Added an Async Task Scheduler to allow tasks to be invoked at certain times and intervals, like 10 minutes past every hour

---
+++ Serpent.Common/Serpent.Common.Async/AsyncTaskScheduler.cs
@@ -0,0 +1,67 @@
+﻿namespace Serpent.Common.Async
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class AsyncTaskScheduler
+    {
+        private readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+        private readonly Func<CancellationToken, Task> funcToExecute;
+
+        private readonly TimeSpan interval;
+
+        private readonly DateTime start;
+
+        public AsyncTaskScheduler(DateTime start, TimeSpan interval, Func<Task> funcToExecute)
+        {
+            this.start = start;
+            this.interval = interval;
+            this.funcToExecute = token => funcToExecute();
+        }
+
+        public AsyncTaskScheduler(DateTime start, TimeSpan interval, Func<CancellationToken, Task> funcToExecute)
+        {
+            this.start = start;
+            this.interval = interval;
+            this.funcToExecute = funcToExecute;
+        }
+
+        public void Start()
+        {
+            Task.Run(
+                async () =>
+                    {
+                        var cancellationToken = this.cancellationTokenSource.Token;
+
+                        while (cancellationToken.IsCancellationRequested == false)
+                        {
+                            var waitingTime = this.GetDelayTime();
+                            await Task.Delay(waitingTime, cancellationToken);
+
+                            // This method should be invoked without await to make the timer accurate
+#pragma warning disable 4014
+                            Task.Run(() => this.funcToExecute(cancellationToken), cancellationToken);
+#pragma warning restore 4014
+                        }
+                    });
+        }
+
+        private TimeSpan GetDelayTime()
+        {
+            if (this.start > DateTime.Now)
+            {
+                return this.start - DateTime.Now;
+            }
+
+            // Get the time aligned to the interval
+            return this.interval - TimeSpan.FromTicks((DateTime.Now - this.start).Ticks % this.interval.Ticks);
+        }
+
+        public void Stop()
+        {
+            this.cancellationTokenSource.Cancel();
+        }
+    }
+}
