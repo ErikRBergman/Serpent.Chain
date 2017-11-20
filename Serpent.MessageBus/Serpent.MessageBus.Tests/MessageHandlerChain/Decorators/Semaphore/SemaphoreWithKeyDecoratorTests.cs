31d7fe984213af409ee62f41a17b87795a29c6e3
Erik Bergman <erik_r_bergman@hotmail.com>
2017-11-20 22:32:20 +0100

    Unit test modified to try to get it to work in travis

--- Serpent.MessageBus/Serpent.MessageBus.Tests/MessageHandlerChain/Decorators/Semaphore/SemaphoreWithKeyDecoratorTests.cs
+++ Serpent.MessageBus/Serpent.MessageBus.Tests/MessageHandlerChain/Decorators/Semaphore/SemaphoreWithKeyDecoratorTests.cs
@@ -11,44 +11,41 @@ namespace Serpent.MessageBus.Tests.MessageHandlerChain.Decorators.Semaphore
 
     public class SemaphoreWithKeyDecoratorTests
     {
-        private const int DelayMultiplier = 5;
-
         [Fact]
         public async Task SemaphoreWithKeyDecorator_MultipleConcurrency_Tests()
         {
             var count = 0;
 
             var func = Create.SimpleFunc<KeyValuePair<int, string>>(
-                b => b.Semaphore(c => c.MaxNumberOfConcurrentMessages(2).KeySelector(m => m.Key))
+                b => b
+                    .SoftFireAndForget()
+                    .Semaphore(c => c.MaxNumberOfConcurrentMessages(2).KeySelector(m => m.Key))
                     .Handler(
                         async m =>
                             {
-                                await Task.Delay(DelayMultiplier * 100);
+                                await Task.Delay(500);
                                 Interlocked.Increment(ref count);
                             }));
 
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-            Task.Run(() => func(new KeyValuePair<int, string>(1, "One")));
-            Task.Run(() => func(new KeyValuePair<int, string>(1, "One")));
-            Task.Run(() => func(new KeyValuePair<int, string>(1, "One")));
-            Task.Run(() => func(new KeyValuePair<int, string>(1, "One")));
+            func(new KeyValuePair<int, string>(1, "One"));
+            func(new KeyValuePair<int, string>(1, "One"));
+            func(new KeyValuePair<int, string>(1, "One"));
+            func(new KeyValuePair<int, string>(1, "One"));
 
-            Task.Run(() => func(new KeyValuePair<int, string>(2, "Two")));
-            Task.Run(() => func(new KeyValuePair<int, string>(2, "Two")));
-            Task.Run(() => func(new KeyValuePair<int, string>(2, "Two")));
-            Task.Run(() => func(new KeyValuePair<int, string>(2, "Two")));
+            func(new KeyValuePair<int, string>(2, "Two"));
+            func(new KeyValuePair<int, string>(2, "Two"));
+            func(new KeyValuePair<int, string>(2, "Two"));
+            func(new KeyValuePair<int, string>(2, "Two"));
 #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
 
-            await Task.Delay(50);
-
+            await Task.Delay(200);
             Assert.Equal(0, count);
 
-            await Task.Delay(DelayMultiplier * 100);
-
+            await Task.Delay(600);
             Assert.Equal(4, count);
 
-            await Task.Delay(DelayMultiplier * 300);
-
+            await Task.Delay(500);
             Assert.Equal(8, count);
         }
 
@@ -58,37 +55,36 @@ namespace Serpent.MessageBus.Tests.MessageHandlerChain.Decorators.Semaphore
             var count = 0;
 
             var func = Create.SimpleFunc<KeyValuePair<int, string>>(
-                b => b.Semaphore(c => c.KeySelector(m => m.Key))
+                b => b
+                    .FireAndForget()
+                    .Semaphore(c => c.KeySelector(m => m.Key))
                     .Handler(
                         async m =>
                             {
-                                await Task.Delay(DelayMultiplier * 100);
+                                await Task.Delay(500);
                                 Interlocked.Increment(ref count);
                             }));
 
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-            Task.Run(() => func(new KeyValuePair<int, string>(1, "One")));
-            Task.Run(() => func(new KeyValuePair<int, string>(1, "One")));
-            Task.Run(() => func(new KeyValuePair<int, string>(1, "One")));
-            Task.Run(() => func(new KeyValuePair<int, string>(1, "One")));
+            func(new KeyValuePair<int, string>(1, "One"));
+            func(new KeyValuePair<int, string>(1, "One"));
+            func(new KeyValuePair<int, string>(1, "One"));
+            func(new KeyValuePair<int, string>(1, "One"));
 
-            Task.Run(() => func(new KeyValuePair<int, string>(2, "Two")));
-            Task.Run(() => func(new KeyValuePair<int, string>(2, "Two")));
-            Task.Run(() => func(new KeyValuePair<int, string>(2, "Two")));
-            Task.Run(() => func(new KeyValuePair<int, string>(2, "Two")));
+            func(new KeyValuePair<int, string>(2, "Two"));
+            func(new KeyValuePair<int, string>(2, "Two"));
+            func(new KeyValuePair<int, string>(2, "Two"));
+            func(new KeyValuePair<int, string>(2, "Two"));
 #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
 
-            await Task.Delay(50);
-
+            await Task.Delay(200);
             Assert.Equal(0, count);
 
-            await Task.Delay(DelayMultiplier * 100);
-
+            await Task.Delay(600);
             Assert.Equal(2, count);
 
-            await Task.Delay(DelayMultiplier * 500);
-
-            Assert.Equal(8, count);
+            await Task.Delay(500);
+            Assert.Equal(4, count);
         }
     }
 }
